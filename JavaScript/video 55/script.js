e9cb720d75153524ae4946bb9b7fbcfd305b09a6
Piyush Raj <raj4piyush@gmail.com>
2025-07-09 21:18:31 +0530

    JavaScript Variables and Data Types

---
+++ JavaScript/video 55/script.js
@@ -0,0 +1,28 @@
+console.log("Hey this is turorial 55.");
+var a = 5;
+// a = a+8; This is allowed 
+const p = 9;
+// p = p+9; This is now allowed
+var b = 6;
+console.log(a+b);
+var c = "Piysuh Raj";
+console.log(typeof(a), typeof(b), typeof(c));
+// Variable in javascript is case sensative
+// Const is used to create a variable which can't be updated in the code
+// Var is global variable but var is global defined
+let x = "Piyush Raj"
+let y = 22;
+let z = 3.55;
+const t = true;
+let r = null;
+let q = undefined;
+console.log(typeof(x), typeof(y), typeof(z), typeof(t), typeof(q), typeof(r))
+
+// JavaScript object is a key value pair
+let o = {
+    "name": "Piyush",
+    "job code": 5600
+};
+console.log(o);
+o.salary = 4243;
+console.log(o);
