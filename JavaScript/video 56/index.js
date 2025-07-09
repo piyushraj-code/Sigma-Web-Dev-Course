8d110dc94bb6f2f77b21e669749786e03525503e
Piyush Raj <raj4piyush@gmail.com>
2025-07-09 22:09:52 +0530

    JavaScript conditionsal

---
+++ JavaScript/video 56/index.js
@@ -0,0 +1,12 @@
+console.log("Hey I am conditional tutorial");
+let age = 45;
+if(age > 18){
+    console.log("You can drive.")
+}
+else{
+    console.log("You can't drive.")
+}
+let a = 5;
+let b = 9;
+let c = a>b? (b-a): (a-b);
+//This is same as else if ladder
