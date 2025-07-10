d869373233835d0be77a9c1a04f14ed4e7ebb657
Piyush Raj <raj4piyush@gmail.com>
2025-07-10 12:31:49 +0530

    JavaScript Functions

---
+++ JavaScript/video 58/index.js
@@ -0,0 +1,17 @@
+function greeting(name){
+    console.log("Hey " + name + " You are nice!")
+
+}
+greeting("Piyush")
+function sum(a, b) {
+    console.log(a+b);
+    return a+b;
+}
+result = sum(10, 5);
+sum(9,4);
+console.log("The sum of 10 and 5 is: "+ result);
+// Arrow Function
+const func1 = (x) =>{
+    console.log("Hey I am an arrow function", x);
+}
+func1(44)
