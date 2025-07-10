64dc8fa91ddb43b719c191c9c33f02c130d85ec9
Piyush Raj <raj4piyush@gmail.com>
2025-07-10 23:18:23 +0530

    JavaScript Strings

---
+++ JavaScript/video 60/index.js
@@ -0,0 +1,11 @@
+console.log("This is a string tutorial")
+let real_name = "Piyush";
+console.log(real_name[2]);
+console.log(real_name.length);
+let friend = "Deepak"
+console.log(`His name is ${real_name} and his friend name is ${friend}`);
+console.log(real_name.toUpperCase());
+console.log(friend.toLowerCase);
+console.log(friend.slice(1,4));
+console.log(friend.replace("De", "P"));
+console.log(friend.concat("Priya"));
