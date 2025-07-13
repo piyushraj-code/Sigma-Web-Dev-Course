f42cc979e144a493a87c29512c68bf5009a0fb2b
Piyush Raj <raj4piyush@gmail.com>
2025-07-13 11:10:37 +0530

    JavaScript Color Box using JS

---
+++ JavaScript/video 70/script.js
@@ -0,0 +1,11 @@
+document.getElementById("box1").style.backgroundColor = "Yellow";
+document.getElementById("box2").style.backgroundColor = "Red";
+document.getElementById("box3").style.backgroundColor = "Brown";
+document.getElementById("box4").style.backgroundColor = "Violet";
+document.getElementById("box5").style.backgroundColor = "Green";
+
+document.getElementById("box1").style.color = "Green";
+document.getElementById("box2").style.color = "Pink";
+document.getElementById("box3").style.color = "Yellow";
+document.getElementById("box4").style.color = "Blue";
+document.getElementById("box5").style.color = "Brown";
